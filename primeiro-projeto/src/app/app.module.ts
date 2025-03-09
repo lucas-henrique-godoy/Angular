71eb9a3e7eecc2f725f50a21d497a5554d03358a
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-03-09 18:19:24 -0300

    Resolvi o  erro de localização do app-cursos, era só no cursos.modulets e usar o exports no CursosCmponent. Ai ele fica visível e pode ser utilizado.

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CursosModule } from './cursos/cursos.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,15 +7,17 @@ import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     MeuPrimeiroComponent,
-    MeuPrimeiro2Component
+    MeuPrimeiro2Component       
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
