dcb4fd214a150a72e7180520ef4ff3a7f24ae9c7
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-03-09 15:52:03 -0300

    Criei exemplos mostrando as diferenças entre javascript e typescript

---
+++ hello-typescript/main.js
@@ -0,0 +1,12 @@
+var minhaVar = 'minha variavel';
+function minhaFuncao(x, y) {
+    return x + y;
+}
+//ES 6 ou ES 2015
+var num = 2;
+var PI = 3.14;
+var numeros = [1, 2, 3];
+numeros.map(function (valor) {
+    return valor * 2;
+});
+numeros.map(function (valor) { return valor * 2; }); //EX 2015
