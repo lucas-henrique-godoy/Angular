38d235bf408ce8e27ae24a5aea2a1e5d61e2fa39
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-03-11 14:35:18 -0300

    Resolvi os problemas que não estavam deixando o projeto ser renderizado, era na main.ts

--- primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -4,7 +4,8 @@ import { Component } from "@angular/core";
     selector: 'meu-primeiro-component',
     template: `
         <p>Meu primeiro component com Angular!</p>
-    `
+    `,
+    standalone: true
 })
 export class MeuPrimeiroComponent {
     
