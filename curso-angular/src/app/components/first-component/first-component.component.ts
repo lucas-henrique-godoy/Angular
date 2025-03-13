3e11edb743408c057160261dfe532cd1fe6ef917
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-03-13 11:21:45 -0300

    Coloquei comentários para explicar a importação de um componente em outro através do seletor

--- curso-angular/src/app/components/first-component/first-component.component.ts
+++ curso-angular/src/app/components/first-component/first-component.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-first-component',
+  selector: 'app-first-component', //Seletor que usamos para improtar um componente em outro
   templateUrl: './first-component.component.html',
   styleUrls: ['./first-component.component.css']
 })
