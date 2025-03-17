404f9eada0168497c21a9524c444e1e7242d64aa
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-03-17 14:48:48 -0300

    Criei um evento onde o  clicar no botão você é redirecionado a uma aba no navegador, no meu exemplo eu usei o site do YouTube.

--- curso-angular/src/app/components/eventos/eventos.component.ts
+++ curso-angular/src/app/components/eventos/eventos.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.css']
 })
 export class EventosComponent implements OnInit {
-
+  show: boolean = true;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  showMessage(): void {
+    this.show = !this.show; //toggle
+  }
+  
+  entrarYoutube(): void{
+    const url = 'http://youtube.com';
+    window.location.href = url;
+  }
+  
 }
