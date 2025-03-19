c2480ade62eb4a437642240954ab60eda92bbb12
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-03-19 17:35:20 -0300

    Criei uma interface e a exportei para mostrar as propriedades no template.  Os dados são de animais, com nome, tipo e idade.

--- curso-angular/src/app/components/list-render/list-render.component.ts
+++ curso-angular/src/app/components/list-render/list-render.component.ts
@@ -1,23 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Animal } from 'src/app/Animal';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent implements OnInit {
-  animals = [
-    {name: "Turca", type: "Dog"},
-    {name: "Tom", type: "Cat"},
-    {name:"Frida", type: "Dog"},
-    {name: "Bob", type: "Horse"},
+  animals: Animal[] = [
+    {name: "Turca", type: "Dog", age: 4},
+    {name: "Tom", type: "Cat", age: 10},
+    {name:"Frida", type: "Dog", age: 5},
+    {name: "Bob", type: "Horse", age: 1},
   ];
 
-   testes = [
-    {name: "Teste", type: "Alguma coisa"},
-    {name: "Teste2", type: "Outra coisa"}    
-   ];
-
+   
   constructor() { }
 
   ngOnInit(): void {
