8fdea99bc5c44f218f0f2e200566307597a174f7
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-03-20 09:41:37 -0300

    Adicionei anotações sobre two way data binding.

--- curso-angular/src/app/components/pipes/pipes.component.ts
+++ curso-angular/src/app/components/pipes/pipes.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class PipesComponent implements OnInit {
   someText = "TESTANDO O PIPE OPERATOR";
 
+  today = new Date();
+
   constructor() { }
 
   ngOnInit(): void {
