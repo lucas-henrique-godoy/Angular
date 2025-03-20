d62d91ffcf94df8225ef630e12f3385e449eeb34
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-03-20 07:22:04 -0300

    Criei um novo componente para aprender pipe operators

--- curso-angular/src/app/app.module.ts
+++ curso-angular/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { EventosComponent } from './components/eventos/eventos.component';
 import { EmitterComponent } from './components/emitter/emitter.component';
 import { ChangeNumberComponent } from './components/change-number/change-number.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
+import { PipesComponent } from './components/pipes/pipes.component';
 
 
 
@@ -24,6 +25,7 @@ import { ListRenderComponent } from './components/list-render/list-render.compon
     EmitterComponent,
     ChangeNumberComponent,
     ListRenderComponent,
+    PipesComponent,
         
   ],
   imports: [
