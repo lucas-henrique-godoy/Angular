402f42a3debf6af9a4b7417a9184a2eb6913c47b
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-03-20 15:14:12 -0300

    Preciso estudar mais os services para entender melhor

--- curso-angular/src/app/components/list-render/list-render.component.ts
+++ curso-angular/src/app/components/list-render/list-render.component.ts
@@ -1,7 +1,8 @@
-//import { Animal } from './../../Animal';
 import { Component, OnInit } from '@angular/core';
 
 import { Animal } from 'src/app/Animal';
+
+import { ListService } from 'src/app/services/list.service';
 
 @Component({
   selector: 'app-list-render',
@@ -24,13 +25,18 @@ export class ListRenderComponent implements OnInit {
    
   animalDetails = '';
 
-  constructor() { }
+  constructor(private listService: ListService) { }
 
   ngOnInit(): void {
   }
 
   showAge(animal: Animal) {
     this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`;
+  }
+
+  removeAnimal(animal: Animal){
+    console.log("Removendo animal...")  
+    this.animals = this.listService.remove(this.animals, animal);  
   }
 
 }
