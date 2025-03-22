5daa287d8fabaaa0ffa8ca48f01455510f198472
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-03-22 18:48:53 -0300

    Aprendi a utilizar as rotas do Angular para realizar a navegação entre as páginas

--- curso-angular/src/app/app-routing.module.ts
+++ curso-angular/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes} from "@angular/router";
 import { FirstComponentComponent } from "./components/first-component/first-component.component";
 import { ListRenderComponent } from "./components/list-render/list-render.component";
+import { TwoWayBindingComponent } from "./components/two-way-binding/two-way-binding.component";
 
 const routes: Routes = [
     {path: '', component: FirstComponentComponent},
-    {path: 'list', component: ListRenderComponent}
+    {path: 'list', component: ListRenderComponent},
+    {path: 'form', component: TwoWayBindingComponent}
 ]
 
 @NgModule({
