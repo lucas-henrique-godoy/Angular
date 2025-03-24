97f7fc2d773a28b3931bceb1ebcaa43dfa961f18
Lucas Henrique Godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-03-24 17:45:25 -0300

    Criei um component igual mostra no vídeo

--- cadastro-pessoas/src/app/app.module.ts
+++ cadastro-pessoas/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PessoaComponent } from './pessoas/pessoa/pessoa.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    PessoaComponent,
   ],
   imports: [
     BrowserModule,
---
+++ cadastro-pessoas/src/app/pessoas/pessoa/pessoa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoa',
+  standalone: false,
+  templateUrl: './pessoa.component.html',
+  styleUrl: './pessoa.component.css'
+})
+export class PessoaComponent {
+
+}
