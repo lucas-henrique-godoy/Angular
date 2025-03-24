ae59cb8d266f2373771633b6fe249f7f331a015a
Lucas Henrique Godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-03-24 17:15:04 -0300

    Preciso criar o formulário

--- cadastro-pessoas/src/app/components/pessoas/pessoas.component.ts
+++ cadastro-pessoas/src/app/components/pessoas/pessoas.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+//import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-pessoas',
@@ -7,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './pessoas.component.css'
 })
 export class PessoasComponent {
+
 
 }
