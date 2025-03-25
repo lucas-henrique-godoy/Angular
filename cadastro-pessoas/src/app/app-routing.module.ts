69bf3eddc0505f5dbb10b6e9ee41e2ae509ffff3
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-03-25 09:02:54 -0300

    Criei a rota para o formulário no app-routing.module.ts

--- cadastro-pessoas/src/app/app-routing.module.ts
+++ cadastro-pessoas/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
+import { PessoaComponent } from './views/pessoas/pessoa/pessoa.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "pessoa", component: PessoaComponent }// Criei a rota para o formulário de cadastro
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
