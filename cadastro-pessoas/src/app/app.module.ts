898c09ab4fd40774343145cf94c84e1cae583200
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-03-25 10:13:50 -0300

    Criei o formulário e as propriedades

--- cadastro-pessoas/src/app/app.module.ts
+++ cadastro-pessoas/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms'; //Necessário importar para usar o FormGroup
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,11 +10,12 @@ import { PessoaComponent } from './views/pessoas/pessoa/pessoa.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PessoaComponent,
+    PessoaComponent, //Importar o componente
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule //Importar o module que será utilizado
   ],
   providers: [],
   bootstrap: [AppComponent]
--- cadastro-pessoas/src/app/views/pessoas/pessoa/pessoa.component.ts
+++ cadastro-pessoas/src/app/views/pessoas/pessoa/pessoa.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-pessoa',
@@ -8,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class PessoaComponent {
 
+  formPessoa: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.formPessoa = fb.group({
+      nome: [''],
+      cpf: [''],
+      dataNascimento: [new Date],
+      participaFidelidade: [''],
+      saldoConta: [0],
+      telefone: [''],
+      cep: [''],
+      rua: [''],
+      bairro: [''],
+      cidade: [''],
+      estado: ['']
+    })
+  }
 }
