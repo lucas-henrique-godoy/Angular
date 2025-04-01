4ba43ce3232dad71fd4dc5437cf63c089a479bc4
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-03-31 23:11:59 -0300

    Criando um rota dinâmica

--- curso-angular/src/app/components/item-detail/item-detail.component.ts
+++ curso-angular/src/app/components/item-detail/item-detail.component.ts
@@ -1,4 +1,7 @@
+import { ListService } from './../../services/list.service';
 import { Component, OnInit } from '@angular/core';
+import { Animal } from 'src/app/Animal';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-item-detail',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-detail.component.css']
 })
 export class ItemDetailComponent implements OnInit {
+  animal? : Animal;
 
-  constructor() { }
+  constructor(private listService: ListService, private route: ActivatedRoute) { 
+    this.getAnimal()
+  }
 
   ngOnInit(): void {
   }
 
+    getAnimal(){ //Acessando o service pegando os dados do  "banco": db.json
+      const id = Number(this.route.snapshot.paramMap.get("id"));
+    }
 }
